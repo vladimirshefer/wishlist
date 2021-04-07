98df2ee2486e6ae5b8f5875c3b37c351a152dcd7
Dmitriy Petrov <62036937+dimkasyan@users.noreply.github.com>
2021-04-08 02:04:57 +0300

    Support items removal
    * add button close
    
    * add handler remove item
    
    * Refactoring
    
    Co-authored-by: Dmitriy Petrov <d.petrov@polymatica.com>
    Co-authored-by: Vladimir Shefer <quameu@gmail.com>

--- src/components/WishlistItemCard.vue
+++ src/components/WishlistItemCard.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="card col-12 wishlist-item">
-    <div class="card-body">
-      <div class="wishlist-item-card">
+  <div class="col-12">
+    <div class="card wishlist-item">
+      <div class="card-body">
+        <div class="wishlist-item-card">
+        </div>
+        <h5 class="card-title">
+          {{ item.name }}
+          <b-dropdown variant="light" size="sm">
+            <b-dropdown-item @click="$emit('remove')">Удалить</b-dropdown-item>
+          </b-dropdown>
+        </h5>
+        <p><b>{{ item.cost }} ₽.</b></p>
+        <b-link class="card-link" target="_blank" v-if="item.link" :href="item.link">Открыть</b-link>
+        <span style="color: gray" v-else>Ссылка отсутствует</span>
       </div>
-      <h5 class="card-title">{{ item.name }}</h5>
-      <p><b>{{ item.cost }} ₽.</b></p>
-      <a class="card-link" target="_blank" v-if="item.link" :href="item.link">Открыть</a>
-      <span style="color: gray" v-else>Ссылка отсутствует</span>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "WishlistItemCard",
   props: {
