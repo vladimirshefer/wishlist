07a0958725e26dd5619d2e5fca20848cfdeda732
Vladimir Shefer <quameu@gmail.com>
2021-04-09 00:09:09 +0300

    Add link to main page on main-logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,10 +2,12 @@
 
   <b-navbar toggleable="lg" type="light" variant="light">
 
-    <b-navbar-brand href="#">
-      <b-avatar square variant="light" :src="logoUrl"/>
-      WishList
-    </b-navbar-brand>
+    <router-link to="/" v-slot="{ href }">
+      <b-navbar-brand :href="href">
+        <b-avatar square variant="light" :src="logoUrl"/>
+        WishList
+      </b-navbar-brand>
+    </router-link>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -20,7 +22,7 @@
           <template #button-content>
             <b-avatar variant="light" :src="user.photoURL"/>
           </template>
-          <b-dropdown-item disabled :href="'/user/'+user.uid">{{ user.displayName || user.email}}</b-dropdown-item>
+          <b-dropdown-item disabled href="#">{{ user.displayName || user.email}}</b-dropdown-item>
           <b-dropdown-item href="#" @click="$emit('signOut')"><b-icon icon="box-arrow-right"/> Выйти</b-dropdown-item>
         </b-nav-item-dropdown>
         <b-nav-item right v-if="!user" @click="$emit('signIn')"><b-icon icon="google"/> Войти</b-nav-item>
