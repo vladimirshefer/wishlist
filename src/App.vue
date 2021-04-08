eb10d4aac1a88543d9a5272d3054ce4565c0d076
Vladimir Shefer <quameu@gmail.com>
2021-04-08 19:18:44 +0300

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="app" class="container">
-    <Navbar :user="user" @signIn="signInWithGoogle" @signOut="signOut"/>
-    <router-view/>
+  <div id="app">
+    <div class="container">
+      <Navbar :user="user" @signIn="signInWithGoogle" @signOut="signOut"/>
+      <router-view/>
+    </div>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import firebase from 'firebase/app'
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
-  components: { Navbar},
+  components: {Footer, Navbar},
   data() {
     return {
       user: firebase.auth().currentUser,
@@ -39,5 +43,14 @@ export default {
 <style>
 .row {
   margin-top: 10px;
+}
+
+html {
+  position: relative;
+  min-height: 100%;
+}
+
+body {
+  margin-bottom: 65px; /* Margin bottom by footer height */
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="footer-contents">
+            <span>© 2021 Vladimir Shefer.</span>
+            <a href="https://github.com/vladimirshefer/wishlist" target="_blank">
+              GitHub
+            </a>
+            <a href=" https://gitter.im/wishlist-app/community" target="_blank">
+              Gitter
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50px; /* Set the fixed height of the footer here */
+  line-height: 30px; /* Vertically center the text there */
+  border-top: 1px solid gray;
+}
+
+.footer-contents > * {
+  margin-right: 15px;
+}
+</style>
