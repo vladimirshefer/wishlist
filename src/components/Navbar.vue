38d4a568cb4769b81ab3b161f457cb753e94e8c0
Vladimir Shefer <quameu@gmail.com>
2021-04-08 22:00:27 +0300

    Fix strings

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,7 +21,7 @@
             <b-avatar variant="light" :src="user.photoURL"/>
           </template>
           <b-dropdown-item disabled :href="'/user/'+user.uid">{{ user.displayName || user.email}}</b-dropdown-item>
-          <b-dropdown-item href="#" @click="$emit('signOut')">Sign Out</b-dropdown-item>
+          <b-dropdown-item href="#" @click="$emit('signOut')"><b-icon icon="box-arrow-right"/> Выйти</b-dropdown-item>
         </b-nav-item-dropdown>
         <b-nav-item right v-if="!user" @click="$emit('signIn')"><b-icon icon="google"/> Войти</b-nav-item>
       </b-navbar-nav>
