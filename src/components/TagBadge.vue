670c17d9b7d683b9e62ff10fc614de320802385c
Vladimir Shefer <quameu@gmail.com>
2021-04-09 13:00:50 +0300

    Refactoring in TagBadge.vue

--- src/components/TagBadge.vue
+++ src/components/TagBadge.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-badge pill class="mr-2 p-2" :variant="getColorForTag(tag)">
+  <b-badge pill class="tag-badge mr-2" :variant="color">
     {{ tag }}
   </b-badge>
 </template>
@@ -15,14 +15,19 @@ export default {
       colors: ["primary", "secondary", "success", "danger", "warning", "info", "dark"],
     }
   },
-  methods: {
-    getColorForTag(value) {
-      return this.colors[value.charCodeAt(0) % this.colors.length];
+  computed: {
+    hashcode() {
+      return Array.from(this.tag).map(it => it.charCodeAt(0)).reduce((a, b) => a + b * 2)
     },
+    color() {
+      return this.colors[this.hashcode % this.colors.length];
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.tag-badge {
+  min-width: 20px;
+}
 </style>
