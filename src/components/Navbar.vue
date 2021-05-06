c0c8e0f660fef24ab631a77f0410c55d93e6f3cb
Josh Tan <38924154+joshtn@users.noreply.github.com>
2021-05-06 13:37:45 +0200

    Use SVG logo in navbar #31
    Replaced png logo with svg logo.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -64,7 +64,7 @@ export default {
   name: "Navbar",
   data() {
     return {
-      logoUrl: require('@/assets/logo.png'),
+      logoUrl: require('@/assets/logo.svg'),
     }
   },
   computed: {
