78ca7c91e46c00b7232027530696d8717ba0eafe
Vladimir Shefer <quameu@gmail.com>
2021-05-10 20:26:46 +0300

    HOTFIX: Try to fix typescript

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from "vue-router";
-import {Store} from "vuex";
 
 require("@/init/bootstrap")
 require("@/init/firebase")
 require("@/init/vue")
 require("@/init/dayjs")
 
-let router: VueRouter = require("@/init/router");
-let store: Store<any> = require("@/store");
+import router from "@/init/router";
+import store from "@/store";
 
 new Vue({
   render: h => h(App),
