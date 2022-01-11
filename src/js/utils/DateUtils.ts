bbd3fe802b44058e246bb4dd17c87bc116616369
Vladimir Shefer <quameu@gmail.com>
2022-01-12 02:51:16 +0300

    DateUtils: Fix format to 24h

--- src/js/utils/DateUtils.ts
+++ src/js/utils/DateUtils.ts
@@ -4,12 +4,12 @@ class DateUtils {
   displayStringOf(date: dayjs.Dayjs): String {
     const now: dayjs.Dayjs = dayjs();
     if (date.year() !== now.year()) {
-      return date.format("DD.MM.YYYY, hh:mm");
+      return date.format("DD.MM.YYYY, HH:mm");
     }
     if (date.date() !== now.date()) {
-      return date.format("DD MMM, hh:mm");
+      return date.format("DD MMM, HH:mm");
     } else {
-      return date.format("hh:mm");
+      return date.format("HH:mm");
     }
   }
 }
